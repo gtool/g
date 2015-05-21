39a1e1bc4d9db66483c33984d02f90dfb2cf250c
Vasily Polovnyov <vasily@polovnyov.ru>
2015-05-21 21:46:49 +0300

    add g-pr command opening current PR (aliased as 'pr')

---
+++ share/g/functions.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+remote_url=$(git config --get remote.origin.url)
+github_repo=$(echo $remote_url | sed 's/^.*github.*[\/|:]\(.*\/.*\)\.git.*/\1/')
+
+function open_github {
+  fs-open "https://github.com/$github_repo/$1"
+}
